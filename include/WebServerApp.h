e6749c7034fc9462033c38404502475dfae08eab
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-09-07 14:55:49 +0300

    Add Ethernet web server interface

---
+++ include/WebServerApp.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <Arduino.h>
+#include <Ethernet.h>
+#include "ScriptEngine.h"
+
+class WebServerApp {
+public:
+    explicit WebServerApp(ScriptEngine& scripts);
+    bool begin();
+    void loop();
+    IPAddress ip() const { return Ethernet.localIP(); }
+
+private:
+    EthernetServer server_{80};
+    ScriptEngine& scripts_;
+
+    static constexpr size_t MAX_BODY = 16384;
+
+    void handleClient(EthernetClient& client);
+    void sendResponse(EthernetClient& client, int code, const char* contentType, const String& body);
+    void sendIndex(EthernetClient& client);
+    String readBody(EthernetClient& client, size_t contentLength);
+};
