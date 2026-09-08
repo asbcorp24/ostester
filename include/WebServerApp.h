5dc6d68571629e0b1f80cad18134ac8da50b56f2
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-09-08 15:45:03 +0300

    Wire persistent ScriptStore into web server

--- include/WebServerApp.h
+++ include/WebServerApp.h
@@ -3,6 +3,7 @@
 #include <Arduino.h>
 #include <STM32Ethernet.h>
 #include "ScriptEngine.h"
+#include "ScriptStore.h"
 
 class WebServerApp {
 public:
@@ -14,6 +15,7 @@ public:
 private:
     EthernetServer server_{80};
     ScriptEngine& scripts_;
+    ScriptStore store_;
 
     static constexpr size_t MAX_BODY = 16384;
 
@@ -21,4 +23,8 @@ private:
     void sendResponse(EthernetClient& client, int code, const char* contentType, const String& body);
     void sendIndex(EthernetClient& client);
     String readBody(EthernetClient& client, size_t contentLength);
+    static String urlDecode(const String& value);
+    static String queryParam(const String& target, const char* key);
+    static String jsonEscape(const String& value);
+    String scriptsJson() const;
 };
