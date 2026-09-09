023b3b6c7de617a0f549374de777204eab09c511
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-09-09 15:42:00 +0300

    Temporarily remove encoder from OLED diagnostics

--- include/LocalUi.h
+++ include/LocalUi.h
@@ -2,7 +2,6 @@
 
 #include <Arduino.h>
 #include <U8g2lib.h>
-#include <STM32encoder.h>
 #include "NetworkSettings.h"
 
 class LocalUi {
@@ -18,23 +17,17 @@ private:
     NetworkSettings& settings_;
     NetworkSettings::Config edit_{};
     U8G2_SSD1306_128X64_NONAME_F_HW_I2C display_;
-    STM32encoder* encoder_ = nullptr;
 
     Screen screen_ = Screen::Home;
     Field editField_ = Field::Ip;
     int menuIndex_ = 0;
     int octet_ = 0;
     int diagPage_ = 0;
-    int32_t lastEncoder_ = 0;
     uint32_t lastDrawMs_ = 0;
     uint32_t lastAutoPageMs_ = 0;
     uint8_t autoDiagPage_ = 0;
     uint8_t oledAddress_ = 0;
     bool oledReady_ = false;
-
-    static constexpr uint32_t ENC_A = PB6;   // TIM4_CH1
-    static constexpr uint32_t ENC_B = PB7;   // TIM4_CH2
-    static constexpr uint32_t ENC_SW = PG8;
 
     uint8_t scanI2c();
     void draw();
